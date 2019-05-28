3de5dde29797079a33167af77bccb9f072b441b5
윤상건 <45253879+c0510gy@users.noreply.github.com>
2019-05-28 10:24:34 +0900

    Create 9.cpp

---
+++ cpp_solutions/9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int ans;
+int main(){
+    for(int a = 1; a < 1000; a++){
+        for(int b = 1; b < 1000; b++){
+            if(a + b >= 1000)
+                continue;
+            int c = 1000 - a - b;
+            if(a * a + b * b == c * c){
+                ans = a * b * c;
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
